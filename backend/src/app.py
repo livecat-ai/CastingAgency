efc42c543ee74c199dbfa891f57ed8301fe7835b
livecat-ai <johnlockwood@me.com>
2020-11-07 10:13:45 +0000

    feat: created many to may relationship between Movie and Actor table.
    also implemented flask migrate

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 database_path = os.environ['DATABASE_URL']
 
@@ -10,30 +11,53 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_path
 db = SQLAlchemy(app)
 
 
-class Movies(db.Model):
-    __tablename__ = "movies"
+migrate = Migrate(app, db)
+movie_cast = db.Table('movie_cast',
+    db.Column('movie_id', db.Integer, db.ForeignKey('movie.id'), primary_key=True),
+    db.Column('actor_id', db.Integer, db.ForeignKey('actor.id'), primary_key=True)
+    )
+
+class Movie(db.Model):
+    __tablename__ = "movie"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    releasedate = db.Column(db.Date, nullable=False)
-    # actors = db.relationship('actors', backref='movie', lazy=True)
+    # releasedate = db.Column(db.Date, nullable=False)
+    actors = db.relationship('Actor', secondary=movie_cast,
+        backref=db.backref('movies', lazy=True))
+
+    # def __init__(self, title, release_date):
+    #     self.title = title
+    #     self.releasedate = release_date
+
+    # def __repr__(self):
+    #     return f'< Movies: {self.title}, {self.releasedate} >'
 
     def __repr__(self):
-        return f'< Movies: {self.title}, {self.releasedate} >'
+        return f'< Movies: {self.title} >'
 
 
-class Actors(db.Model):
-    __tablename__ = "actors"
+class Actor(db.Model):
+    __tablename__ = "actor"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    age = db.Column(db.Date, nullable=False)
-    gender = db.Column(db.Integer, nullable=False)
+    # age = db.Column(db.Integer, nullable=False)
+    # gender = db.Column(db.String, nullable=False)
     # movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
 
+    # def __init__(self, name, age, gender, movie_id):
+    #     self.name = name
+    #     self.age = age
+    #     self.gender = gender
+    #     self.movie_id = movie_id
+
+    # def __repr__(self):
+    #     return f'< Actors: {self.name}, {self.age}, {self.gender} >'
+
     def __repr__(self):
-        return f'< Actors: {self.name}, {self.age}, {self.gender} >'
+        return f'< Actors: {self.name}>'
 
 
-db.create_all()
+# db.create_all()
 
 @app.route('/')
 def index():
