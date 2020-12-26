c1152ed2819f03938c8d79637650fa71214ef121
Livecate <johnlockwood@me.com>
2020-12-26 04:25:03 +0000

    chore: cleanup deleted files

--- setup_test.sh
+++
@@ -1,6 +0,0 @@
-export FLASK_APP=~/repos/CastingAgency/backend/src/app.py
-export FLASK_ENV=development
-export TEST_DATABASE_URL=postgres://johnlockwood:jal109@localhost:5432/test_casting
-export TESTING=True
-
-
